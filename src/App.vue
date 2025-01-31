f16bf5a08e32523c7ee72f7fcca38f315b66029a
Bamos Guinley <guinleyb@gmail.com>
2025-01-31 11:42:57 +0100

    installation de lucide icon & tailwind css

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
- <p>hello sur mon appli de dessin</p>
-
   <RouterView />
 </template>
 
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>hellow let's draw</h1>
+<h1 class="text-red-500">hellow let's draw</h1>
 </template>
 <script setup lang="ts">
 </script>
