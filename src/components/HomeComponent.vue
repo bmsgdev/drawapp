ec1dcdb5d938f4e970f775254de5be0e3f820adc
Bamos Guinley <guinleyb@gmail.com>
2025-01-31 12:13:33 +0100

    add icons

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,5 +1,14 @@
-<template>
-<h1 class="text-red-500">hellow let's draw</h1>
-</template>
 <script setup lang="ts">
+import CanvasComponent from "./CanvasComponent.vue";
+import ToolBarComponent from "./ToolBarComponent.vue";
 </script>
+<template>
+  <div class="flex w-full">
+    <div class="w-[25%]">
+      <ToolBarComponent />
+    </div>
+    <div class="w-[75%] bg-blue-500">
+      <CanvasComponent />
+    </div>
+  </div>
+</template>
---
+++ src/components/ToolBarComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Download, Eraser, Paintbrush, RotateCcw } from "lucide-vue-next";
+</script>
+<template>
+  <div>
+    <div>
+      <div>
+         <button>
+            <Paintbrush />
+            <Eraser />
+            <RotateCcw />
+            <Download/>
+         </button>
+      </div>
+    </div>
+  </div>
+</template>
