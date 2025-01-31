5af297ef20e4a324a5b39f567061250a47b4beb1
Bamos Guinley <guinleyb@gmail.com>
2025-01-31 14:54:37 +0100

    add canvas

--- src/components/CanvasComponent.vue
+++ src/components/CanvasComponent.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+const canvas = ref<HTMLCanvasElement|null>();
+const ctx = canvas.value?.getContext("2d");
+let coord: Object = { x: 0, y: 0 };
+
+onMounted(() => {
+console.log(canvas.value)
+});
+
 </script>
 <template>
-
+<canvas ref="canvas" id="canvas" class=" w-full h-full"></canvas>
 </template>
--- src/components/ToolBarComponent.vue
+++ src/components/ToolBarComponent.vue
@@ -9,27 +9,27 @@ import { Download, Eraser, Paintbrush, RotateCcw } from "lucide-vue-next";
       <div class="flex flex-col gap-2 justify-center p-5">
          <!-- first button groupe -->
         <div class="flex items-center justify-center gap-2">
-          <button class=" flex items-center justify-center bg-blue-300 w-15 h-15 border border-white rounded-md">
+          <button class=" flex items-center justify-center bg-blue-300 w-15 h-15 border border-white rounded-md cursor-pointer">
             <Paintbrush :size="25" />
           </button>
-          <button class=" flex items-center justify-center bg-red-300 w-15 h-15 border border-white rounded-md">
+          <button class=" flex items-center justify-center bg-red-300 w-15 h-15 border border-white rounded-md cursor-pointer">
             <Eraser />
           </button>
         </div>
         <div class="flex flex-col">
           <label for="color">Color</label>
-          <input class="w-full h-10 " type="color" />
+          <input class="w-full h-10 cursor-pointer" type="color" />
         </div>
-        <div class=" flex flex-col w-full">
+        <div class=" flex flex-col w-full ">
           <label for="color">Range</label>
-          <input class="w-full h-4 " type="range" min="1" max="50" value="1"/>
+          <input class="w-full h-4 cursor-pointer" type="range" min="1" max="50" value="1"/>
         </div>
        <div class="flex flex-col items-center justify-center gap-2">
-          <button  class="hover:text-white flex items-center justify-center w-full h-10 bg-blue-300 hover:bg-blue-500 gap-2 rounded-md">
+          <button  class="hover:text-white flex items-center cursor-pointer justify-center w-full h-10 bg-blue-300 hover:bg-blue-500 gap-2 rounded-md">
           <RotateCcw icon class=" hover:text-white"/>
           <p class="font-bold">Clear</p>
         </button>
-        <button class="flex items-center justify-center w-full h-10 bg-green-300 hover:bg-green-500 gap-2 rounded-md">
+        <button class="flex items-center justify-center cursor-pointer w-full h-10 bg-green-300 hover:bg-green-500 gap-2 rounded-md">
           <Download />
           <p>Download</p>
         </button>
