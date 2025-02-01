1caf85be9421d3534c2f42fed41c538a4041d819
Bamos Guinley <guinleyb@gmail.com>
2025-02-01 09:36:14 +0100

    correctif du bug draw

--- src/components/CanvasComponent.vue
+++ src/components/CanvasComponent.vue
@@ -3,22 +3,24 @@ import { ref, onMounted } from "vue";
 const canvas = ref<HTMLCanvasElement | null>();
 
 let lastCoord = ref<{ x: number; y: number } | null>(null);
+let isDrawing = ref(false);
 const startDrawing = (e: MouseEvent) => {
-
+  isDrawing.value = true;
+  const rect = canvas.value?.getBoundingClientRect();
+  if (!rect) return;
   lastCoord.value = {
-    x: e.clientX,
-    y: e.clientY,
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top,
   };
 };
 
 const stopDrawing = (e: MouseEvent) => {
-  //   lastCoord.value = {
-  //     x: 0,
-  //     y: 0,
-  //   };
+  isDrawing.value = false;
+  lastCoord.value = null;
 };
 
 const draw = (e: MouseEvent) => {
+  if (!isDrawing.value) return;
   const ctx = canvas.value?.getContext("2d");
   const rect = canvas.value?.getBoundingClientRect();
   if (!rect) return;
@@ -42,33 +44,34 @@ const resize = () => {
   canvas.value.height = window.innerHeight;
   canvas.value.width = window.innerWidth - toolbar.clientWidth;
 };
+window.addEventListener("resize", resize);
 onMounted(() => {
-
-    resize();
-//     const ctx = canvas.value?.getContext("2d");
-//   if (ctx) {
-//     // créer un rectangle avec l'object canvas
-//     // ctx.fillStyle = "green";
-//     // ctx.fillRect(40, 10, 100, 100);
-//     // ctx.strokeStyle = "red";
-//     // ctx.strokeRect(40, 10, 100, 100);
-//     // créer des traits
-//     // ctx.beginPath();
-//     // ctx.moveTo(50, 50);
-//     // ctx.lineTo(200, 50);
-//     // ctx.lineTo(200, 200);
-//     // ctx.stroke();
-//   }
-//   console.log(canvas.value);
+  resize();
+  //     const ctx = canvas.value?.getContext("2d");
+  //   if (ctx) {
+  //     // créer un rectangle avec l'object canvas
+  //     // ctx.fillStyle = "green";
+  //     // ctx.fillRect(40, 10, 100, 100);
+  //     // ctx.strokeStyle = "red";
+  //     // ctx.strokeRect(40, 10, 100, 100);
+  //     // créer des traits
+  //     // ctx.beginPath();
+  //     // ctx.moveTo(50, 50);
+  //     // ctx.lineTo(200, 50);
+  //     // ctx.lineTo(200, 200);
+  //     // ctx.stroke();
+  //   }
+  //   console.log(canvas.value);
 });
 </script>
 <template>
   <canvas
-  class="border border-blue-500"
+    class="border border-blue-500"
     ref="canvas"
     id="canvas"
     @mousedown="startDrawing"
     @mousemove="draw"
     @mouseup="stopDrawing"
+    @mouseout="stopDrawing"
   ></canvas>
 </template>
